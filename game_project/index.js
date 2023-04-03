708793d6db51e18868f79015e147ebc01d7ea555
yash1017 <103556819+yash1017@users.noreply.github.com>
2023-04-03 22:32:37 +0530

    Update index.js

--- game_project/index.js
+++ game_project/index.js
@@ -1,77 +1,6 @@
-// const cards = document.querySelectorAll(".card");
-
-// var firstCard,secondCard,isFlipped=false;
-
-
-// cards.forEach( (card)=>card.addEventListener( "click" , flip))
-
-
-// function flip(){
-  
-//   this.classList.add("flip");
-//   if(!isFlipped)
-//   {
-//      isFlipped=true;
-//      firstCard=this;
-//   }
-//   else{
-    
-//     secondCard= this;
-//     console.log(firstCard);
-//     console.log(secondCard);
-//     checkIt();
-//   }
-// }
-
-
-// function checkIt(){
-//   if(firstCard.dataset.image === secondCard.dataset.image)
-//   {
-//     success();
-//   }
-//   else{
-//     failure();
-//   }
-// }
-
-// function success(){
-//   firstCard.removeEventListener("click",flip);
-//   secondCard.removeEventListener("click",flip);
-
-//   setTimeout( ()=>{
-//     firstCard.classList.remove("flip");
-//     secondCard.classList.remove("flip");
-//     reset();
-//   },2000);
-
-// }
-
-// var failure =()=>{
-//   setTimeout( ()=>{
-//   firstCard.classList.remove("flip");
-//   secondCard.classList.remove("flip");
-//   reset();
-//   },1000)
-// }
-
-// function reset(){
-//   isFlipped = false;
-//   firstCard = null;
-//   secondCard = null;
-// }
-
-// (function shuffle(){
-//   cards.forEach( (card)=>{
-//     var index = Math.floor(Math.random() * 16);
-//     card.style.order= index;
-//   })
-// })();
-
-
-
-
 const cards =document.querySelectorAll(".card");
-var point = document.querySelector(".win")
+var point = document.querySelector(".text");
+const cont = document.querySelector(".win");
 
 var text = document.createTextNode("CAN YOU WIN");
 point.appendChild(text);
@@ -107,6 +36,8 @@ function checkOut(){
 function success(){
   // point.innerHTML=null;
 
+  cont.classList.add("won");
+
   point.removeChild(text);
   text = document.createTextNode("YOU WON");
   point.appendChild(text);
@@ -135,6 +66,7 @@ function reset(){
   secondCard=null;
   isFlipped=false;
   point.removeChild(text);
+  cont.classList.remove("won");
   text= document.createTextNode("CAN YOU WIN");
   point.appendChild(text); 
 }
